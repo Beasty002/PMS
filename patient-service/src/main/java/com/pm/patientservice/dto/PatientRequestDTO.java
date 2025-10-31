eef6b6c4e5c499da326e7189527a1fa222458888
Beasty002 <xasbin12@gmail.com>
2025-10-31 17:28:35 +0545

    feat: completed patient service

--- patient-service/src/main/java/com/pm/patientservice/dto/PatientRequestDTO.java
+++ patient-service/src/main/java/com/pm/patientservice/dto/PatientRequestDTO.java
@@ -1,5 +1,7 @@
 package com.pm.patientservice.dto;
 
+import com.pm.patientservice.dto.validators.CreatePatientValidationGroup;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -8,19 +10,24 @@ import jakarta.validation.constraints.Size;
 public class PatientRequestDTO {
     @NotBlank(message = "Name is Required")
     @Size(max = 100 , message = "Name cannot exceed 100 characters.")
+    @Schema(example = "Beasty")
     private String name;
 
     @NotBlank(message = "Email is required")
     @Email(message = "Email should be valid")
+    @Schema(example = "something@mail.com")
     private String email;
 
     @NotBlank(message = "Address is required")
+    @Schema(example = "Pokhara")
     private String address;
 
     @NotBlank(message = "Date of Birth is required")
+    @Schema(example = "2004/03/13")
     private String dateOfBirth;
 
-    @NotNull(message = "Registered date is required")
+    @NotBlank(groups = CreatePatientValidationGroup.class, message = "Registered date is required")
+    @Schema(example = "2070/12/30")
     private String registeredDate;
 
     public String getName() {
